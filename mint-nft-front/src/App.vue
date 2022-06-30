3cd64da62cf72ee445da101436690c4389b912eb
novickay <novickaya@sputnikfund.ru>
2022-06-30 13:17:47 +0700

    additional challenges

--- mint-nft-front/src/App.vue
+++ mint-nft-front/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import './assets/styles/App.css';
 import HelloWorld from './components/HelloWorld.vue'
+
+//https://buildspace.so/p/mint-nft-collection/lessons/finish-web-app
 </script>
 
 <template>
--- mint-nft-front/src/components/HelloWorld.vue
+++ mint-nft-front/src/components/HelloWorld.vue
@@ -8,45 +8,48 @@ defineProps<{
 }>()
 const CONTRACT_ADDRESS = "0x311970bA54385ae9bD68287843B974c6525d7c64";
 
-
-
 const currentAccount = ref()
+const isMining = ref(false)
+const isMintDone = ref(false)
 
-// Setup our listener.
+const info = ref('')
+const error = ref('')
+
+
 const setupEventListener = async () => {
-  // Most of this looks the same as our function askContractToMintNft
+  error.value = ""
   try {
     const { ethereum } = window;
 
     if (ethereum) {
-      // Same stuff again
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-      // creates the connection to our contract
       const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-      // THIS IS THE MAGIC SAUCE.
+
       // This will essentially "capture" our event when our contract throws it.
       // If you're familiar with webhooks, it's very similar to that!
       connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
         console.log(from, tokenId.toNumber())
         alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
       });
-
-      console.log("Setup event listener!")
-
     } else {
+      error.value = "Ethereum object doesn't exist!"
       console.log("Ethereum object doesn't exist!");
     }
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
+    error.value = err.message
+    info.value = ""
   }
 }
 
 const checkIfWalletIsConnected = async () => {
+  error.value = ""
   const { ethereum } = window;
 
   if (!ethereum) {
+    error.value = "Make sure you have metamask!"
     console.log("Make sure you have metamask!");
     return;
   } else {
@@ -68,6 +71,7 @@ const checkIfWalletIsConnected = async () => {
 }
 
 const connectWallet = async () => {
+  error.value = ""
   try {
     const { ethereum } = window;
 
@@ -80,25 +84,26 @@ const connectWallet = async () => {
 
     let chainId = await ethereum.request({ method: 'eth_chainId' });
     console.log("Connected to chain " + chainId);
-
-    // String, hex code of the chainId of the Rinkebey test network
     const rinkebyChainId = "0x4";
     if (chainId !== rinkebyChainId) {
       alert("You are not connected to the Rinkeby Test Network!");
+      error.value = "You are not connected to the Rinkeby Test Network!"
     } else {
+      info.value = "Connected!"
       console.log("Connected", accounts[0]);
       currentAccount.value = accounts[0]
       setupEventListener()
     }
-
-
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
+    error.value = err.message
+    info.value = ""
   }
 }
 
 const askContractToMintNft = async () => {
-
+  error.value = ""
+  info.value = ""
   try {
     const { ethereum } = window;
 
@@ -107,23 +112,35 @@ const askContractToMintNft = async () => {
       const signer = provider.getSigner();
       const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
+      info.value = "Going to pop wallet now to pay gas... \n"
       console.log("Going to pop wallet now to pay gas...")
       let nftTxn = await connectedContract.makeAnEpicNFT();
 
+      isMining.value = true
+      info.value = "Mining...please wait. \n"
       console.log("Mining...please wait.")
       await nftTxn.wait();
+      isMining.value = false
 
+      // link
+      info.value = `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
+      isMintDone.value = true
       console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
 
     } else {
+      error.value = "Ethereum object doesn't exist!"
       console.log("Ethereum object doesn't exist!");
     }
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    console.log(err)
+    error.value = err.message
+    info.value = ""
   }
 }
 
 onMounted(() => {
+  error.value = ""
+  info.value = ""
   checkIfWalletIsConnected()
 })
 </script>
@@ -136,12 +153,23 @@ onMounted(() => {
           Each unique. Each beautiful. Discover your NFT today.
         </p>
         <button v-if="!currentAccount" class="cta-button connect-wallet-button" @click="connectWallet">Connect to Wallet</button>
-        <button v-else @click="askContractToMintNft" className="cta-button connect-wallet-button">
+        <button v-else @click="askContractToMintNft" class="cta-button mint-button">
           Mint NFT
         </button>
-      </div>
-      <div class="footer-container">
 
+      </div>
+
+      <div class="loader" v-if="isMining">Minting...</div>
+      <div class="footer-container">
+<!--        <div v-if="isMintDone">-->
+<!--          <p>Minted</p>-->
+<!--          INSERT_TOKEN_ID_HERE -->
+<!--          <a :href=`https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}` class="cta-button collection-button">-->
+<!--            🌊 View Collection on OpenSea-->
+<!--          </a>-->
+<!--        </div>-->
+        <!--        <p class="error" v-if="error">Error: {{ error }}</p>-->
+<!--        <p class="info" v-if="info">Info: <br /> {{ info }}</p>-->
       </div>
     </div>
 
@@ -220,6 +248,9 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   padding-bottom: 30px;
+
+  color: white;
+  font-size: 25px;
 }
 
 
@@ -257,4 +288,44 @@ onMounted(() => {
     background-position: 0% 50%;
   }
 }
+
+
+
+
+.loader {
+  animation: spinningColor 1.5s ease-in-out infinite;
+  margin: 50px auto;
+  border: 5px double #f0eff5;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+
+}
+
+
+
+@keyframes spinningColor {
+  0% {
+    transform: rotate(360deg);
+    border-top:5px dashed #ff6fdf;
+    border-bottom:5px dashed #35aee2;
+  }
+  25% {
+    border-top:5px dashed #ff6fdf;
+    border-bottom:5px dashed #35aee2;
+  }
+  50% {
+    border-top:5px dashed #ff6fdf;
+    border-bottom:5px dashed #35aee2;
+  }
+  75% {
+    border-top:5px dashed #ff6fdf;
+    border-bottom:5px dashed #35aee2;
+  }
+  100% {
+    border-top:5px dashed #ff6fdf;
+    border-bottom:5px dashed #35aee2;
+  }
+}
+
 </style>
